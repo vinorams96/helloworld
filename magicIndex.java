a2375f86831c4e8607bbd89e2f5339400ac87093
vinorams96 <vinorams96@gmail.com>
2016-09-10 18:35:54 +0530

    Update magicIndex.java

--- magicIndex.java
+++ magicIndex.java
@@ -2,13 +2,13 @@ import java.util.Scanner;
 public class magicIndex {
 	public static void main(String[] args) {
 		Scanner input=new Scanner(System.in);
-		int siz=input.nextInt();
-		if(siz<0)
+		int size=input.nextInt();
+		if(size<0)
 			System.out.print("Error");
 		int[] arr=new int[siz];
-		for(int i=0;i<siz;i++)
+		for(int i=0;i<size;i++)
 			arr[i]=input.nextInt();
-		for(int i=0;i<siz;i++)
+		for(int i=0;i<size;i++)
 			if(i==arr[i])
 			System.out.print(i );
 	}
