574e3f79c8e47a8bb1d0cff24e510c13639c69c8
vinorams96 <vinorams96@gmail.com>
2016-09-10 18:30:41 +0530

    Create revnostr.cpp

---
+++ revnostr.cpp
@@ -0,0 +1,27 @@
+#include<iostream.h>
+
+int main()
+{
+char str[20];
+int i,len=0;
+
+cout<<"\nENTER YOUR STRING:";
+cin>>str;
+if((str[0]>'A' && str[0]<'Z') && (str[0]>'a' && str[0]<'z')){
+for(i=0;str[i]!='\0';i++)
+{
+len++;
+}
+for(;len>=z0;len--)
+{
+cout<<str[len];
+}}
+else
+cout<<"Error";
+
+return 0;
+}
+
+    Contact GitHub API Training Shop Blog About 
+
+    © 2016 GitHub, I
